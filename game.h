f970b2d064b14ab16d1589cdf099723e735210d5
nouman0103 <nouman0103@gmail.com>
2023-04-28 20:38:41 +0500

    implement addons

--- game.h
+++ game.h
@@ -3,6 +3,7 @@
 #include <time.h>
 #include "player.h"
 #include "Enemy.h"
+#include "Addons.h"
 // #include "Bullet.h"
 
 
@@ -18,6 +19,8 @@ public:
     // add other game attributes
     Enemy *E; // enemy
     Bullet *b; // bullet
+    Addon *addon; // addons
+    int addonCount = 0;
 
     
 
@@ -33,6 +36,55 @@ public:
         background.setTexture(bg_texture);
         background.setScale(2.25, 1.35);
     }
+    void checkAndSpawnAddons(int &addonTimer)
+    {
+        // call checkAndSpawnAddons every 10 seconds
+        if(addonTimer < 10)
+        {
+            return;
+        }
+        addonTimer = 0;
+        // theres a 1 in 100 chance of spawning an addon
+        int spawnChance = rand() % 100 + 1;
+        if(spawnChance != 1)
+        {
+            return;
+        }
+        if(addonCount < 1)
+        {
+            // spawn a random addon between 1 and 4
+            int addonType = rand() % 4 + 1;
+            if(addonType == 1)
+            {
+                addon = new PowerUp();
+            }
+            else if(addonType == 2)
+            {
+                addon = new Fire();
+            }
+            else if(addonType == 3)
+            {
+                addon = new Danger();
+            }
+            else if(addonType == 4)
+            {
+                addon = new Lives();
+            }
+            addonCount++;
+        }
+    }
+
+    void checkAndRemoveAddons()
+    {
+        if(addon->getSprite().getPosition().y > 700)
+        {
+            delete addon;
+            addonCount--;
+            addon = nullptr;
+        }
+    }
+
+
     void start_game()
     {
         srand(time(0));
@@ -40,12 +92,14 @@ public:
       
         Clock clock;
         float timer = 0;
+        float addonTimer = 0;
 
         while (window.isOpen())
         {
             float time = clock.getElapsedTime().asSeconds();
             clock.restart();
             timer += time;
+            addonTimer += time;
             Event e;
             while (window.pollEvent(e))
             {
@@ -70,18 +124,33 @@ public:
             E->move();
             b->move();
             b->setPos(p->sprite.getPosition().x + 20, p->sprite.getPosition().y);
+            if(addon != nullptr)
+            {
+                addon->move();
+            }
             //
+
+            
+            checkAndSpawnAddons(addonTimer);
+            checkAndRemoveAddons();
+            //////////////////////////////////////////////////
 
             
           
 
             //////////////////////////////////////////////
 
+            
+
             window.clear(Color::Black); // clears the screen
             window.draw(background);    // setting background
             window.draw(p->sprite);     // setting player on screen
             window.draw(E->getSprite()); // setting enemy on screen
             window.draw(b->getSprite()); // setting bullet on screen
+            if(addon != nullptr)
+            {
+                window.draw(addon->getSprite());
+            }
            
             window.display(); // Displying all the sprites
         }
